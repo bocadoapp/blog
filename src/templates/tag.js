80d282c89c54c5d6a1e30ce4111bf012e68b5165
manelgarcia <manelet@gmail.com>
2020-04-06 17:55:46 +0200

    fixes

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -5,38 +5,39 @@ import Layout from '../components/Layout'
 import PostList from '../components/PostList'
 
 const Tag = props => {
-  const { data, pageContext } = props
-  const { edges: posts, totalCount } = data.allWordpressPost
-  const { title: siteTitle } = data.site.siteMetadata
-  const { name: tag } = pageContext
-  const title = `${totalCount} post${
-    totalCount === 1 ? '' : 's'
-  } with the tag ${tag}`
+  return null
+  // const { data, pageContext } = props
+  // const { edges: posts, totalCount } = data.allWordpressPost
+  // const { title: siteTitle } = data.site.siteMetadata
+  // const { name: tag } = pageContext
+  // const title = `${totalCount} post${
+  //   totalCount === 1 ? '' : 's'
+  // } with the tag ${tag}`
 
-  return (
-    <Layout>
-      <Helmet title={`${tag} | ${siteTitle}`} />
-      <PostList posts={posts} title={title} />
-    </Layout>
-  )
+  // return (
+  //   <Layout>
+  //     <Helmet title={`${tag} | ${siteTitle}`} />
+  //     <PostList posts={posts} title={title} />
+  //   </Layout>
+  // )
 }
 
 export default Tag
 
-export const pageQuery = graphql`
-  query TagPage($slug: String!) {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allWordpressPost {
-      totalCount
-      edges {
-        node {
-          ...PostListFields
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query TagPage($slug: String!) {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     allWordpressPost {
+//       totalCount
+//       edges {
+//         node {
+//           ...PostListFields
+//         }
+//       }
+//     }
+//   }
+// `
