80c313fc509947574539c84a337be29bade93911
Travis CI User <travis@example.org>
2020-04-25 16:33:01 +0000

    Updates

---
+++ component---src-pages-quienes-somos-js-1d6be7b16cd93ae1cba1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"+74L":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),c=n.n(a);t.default=function(){return c.a.createElement("div",{className:"flex justify-center items-center w-full py-3 px-6 md:px-32 lg:px-64"},c.a.createElement("h1",{className:"title"},"About"))}}}]);
+//# sourceMappingURL=component---src-pages-quienes-somos-js-1d6be7b16cd93ae1cba1.js.map
